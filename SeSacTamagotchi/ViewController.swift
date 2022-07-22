89933f06b6c8d2aaf575a10c084b927f6caea2f4
DongHee-Sin <voslr0601@naver.com>
2022-07-22 14:33:04 +0900

    Initial Commit

---
+++ SeSacTamagotchi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSacTamagotchi
+//
+//  Created by 신동희 on 2022/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
